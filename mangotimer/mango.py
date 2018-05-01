82803b3826cd743586add28d8891fa893a47eaf9
Pablo Olmos <polmos@illumina.com>
2018-05-01 21:06:58 +0100

    Added new implementation RealTimeLine and modified RecurrentAlarm slightly to work with datetimes

--- mangotimer/mango.py
+++ mangotimer/mango.py
@@ -1,16 +1,17 @@
+import datetime
 
 
 class RecurrentAlarm(object):
 
     def __init__(self):
-        self._when = None
+        self._every = None
         self._trigger_time = None
         self._triggered = False
         self._snoozed = False
         self._postpone_by = 0
 
     def set_every(self, minutes):
-        self._when = minutes
+        self._every = minutes
 
     def is_triggered(self):
         return self._triggered
@@ -26,16 +27,15 @@ class RecurrentAlarm(object):
 
     def notify(self, current_time):
         if self._trigger_time is None:
-            self._trigger_time = current_time + self._when
-        else:
-            self._trigger_time = self._trigger_time - current_time
+            self._trigger_time = current_time + self._every
 
         if self._snoozed:
             self._trigger_time += self._postpone_by
 
-        if self._trigger_time <= 0:
+        if self._trigger_time <= current_time:
             self._triggered = True
-            self._trigger_time = current_time + self._when
+            self._trigger_time = current_time + self._every
+            print('BEEP! BEEP!')
 
 
 class FakeTimeline(object):
@@ -51,6 +51,29 @@ class FakeTimeline(object):
     def run_for(self, minutes):
         self._current_time += minutes
         self.monitor()
+
+    def monitor(self):
+        for alarm in self._alarms:
+            alarm.notify(self._current_time)
+
+
+class RealTimeline(object):
+
+    def __init__(self):
+        self._alarms = []
+        self._current_time = datetime.datetime.now()
+        self._time_increment = None
+
+    def add(self, alarm):
+        self._alarms.append(alarm)
+        alarm.notify(self._current_time)
+
+    def run(self):
+        while True:
+            now = datetime.datetime.now()
+            self._time_increment = now - self._current_time
+            self._current_time = now
+            self.monitor()
 
     def monitor(self):
         for alarm in self._alarms:
